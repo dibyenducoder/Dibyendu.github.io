d32fe1d139d9dda176e9fcd5b76213731233c47d
Dibyendu Mukhopadhyay <44434618+dibyenducoder@users.noreply.github.com>
2020-04-07 15:31:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,41 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+let body = document.body;
+let toggleBtn = document.querySelector(".toggle-btn");
+let currentTheme = localStorage.getItem("currentTheme");
+
+if(currentTheme)
+{
+	body.classList.add("dark-theme");
+}
+// toggleBtn.addEventListener("click",function()
+// {
+// 	body.classList.toggle("dark-theme");
+
+// 	if(body.classList.contains("dark-theme"))
+// 	{
+// 		localStorage.setItem("currentTheme","themeActive");
+// 	}
+// 	else
+// 	{
+// 		localStorage.removeItem("currentTheme");
+
+// 	}
+// })
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark-theme');
+	if(body.classList.contains("dark-theme"))
+	{
+		localStorage.setItem("currentTheme","themeActive");
+	}
+	else
+	{
+		localStorage.removeItem("currentTheme");
+
+	}
+});
